2e2d7a3bb50d8ac23da06e32e845e1085a7d5542
AboodZahran4 <zahranabood62@gmail.com>
2026-07-25 23:50:50 +0300

    chore: add Celery/Redis infrastructure and run containers as non-root

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Redis + Celery
+REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
+
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
